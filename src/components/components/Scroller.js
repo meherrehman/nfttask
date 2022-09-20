035dc579da615dd8295b3791f4950d0f45e84c74
Muhammad Ans <75665100+Muhammadansriaz@users.noreply.github.com>
2022-09-20 10:00:37 -0700

    Classes changed

--- src/components/components/Scroller.js
+++ src/components/components/Scroller.js
@@ -6,12 +6,12 @@ const Scroller = () => {
         <div style={{ background: "black", display: "block" }}>
             <div className="MuiBox-root jss93 ticker-banner" bis_skin_checked={1}>
                 <marquee speed={50} loop="">
-                <div className="MuiBox-root jss94 jss6" bis_skin_checked={1}>
-                    <div className="MuiBox-root jss95 jss7" bis_skin_checked={1}>
-                        <div className="MuiBox-root jss96 jss8" bis_skin_checked={1}>
+                <div className="MuiBox-root jss94 outer_box" bis_skin_checked={1}>
+                    <div className="MuiBox-root jss95 inner_box" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss96 big" bis_skin_checked={1}>
                             Bored Ape Laser Eyes Club
                         </div>
-                        <div className="MuiBox-root jss97 jss9" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss97 inner_box" bis_skin_checked={1}>
                             <div className="MuiBox-root jss98 jss11" bis_skin_checked={1}>
                                 <img
                                     src="/img/ethers-logo.png"
@@ -25,11 +25,11 @@ const Scroller = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="MuiBox-root jss100 jss7" bis_skin_checked={1}>
-                        <div className="MuiBox-root jss101 jss8" bis_skin_checked={1}>
+                    <div className="MuiBox-root jss100 inner_box" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss101 big" bis_skin_checked={1}>
                             African Statut
                         </div>
-                        <div className="MuiBox-root jss102 jss9" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss102 inner_box" bis_skin_checked={1}>
                             <div className="MuiBox-root jss103 jss11" bis_skin_checked={1}>
                                 <img
                                     src="/img/ethers-logo.png"
@@ -38,17 +38,17 @@ const Scroller = () => {
                                 />
                                 0.00075
                             </div>
-                            <div className="MuiBox-root jss104 jss11" bis_skin_checked={1}>
+                            <div className="MuiBox-root jss104 space" bis_skin_checked={1}>
                                 0%
                             </div>
                         </div>
                     </div>
-                    <div className="MuiBox-root jss105 jss7" bis_skin_checked={1}>
-                        <div className="MuiBox-root jss106 jss8" bis_skin_checked={1}>
+                    <div className="MuiBox-root jss105 inner_box" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss106 big" bis_skin_checked={1}>
                             CyberGods 🔥
                         </div>
-                        <div className="MuiBox-root jss107 jss9" bis_skin_checked={1}>
-                            <div className="MuiBox-root jss108 jss11" bis_skin_checked={1}>
+                        <div className="MuiBox-root jss107 inner_box" bis_skin_checked={1}>
+                            <div className="MuiBox-root jss108 space" bis_skin_checked={1}>
                                 <img
                                     src="/img/ethers-logo.png"
                                     alt="#"
@@ -56,7 +56,7 @@ const Scroller = () => {
                                 />
                                 0.0009
                             </div>
-                            <div className="MuiBox-root jss109 jss11" bis_skin_checked={1}>
+                            <div className="MuiBox-root jss109 space" bis_skin_checked={1}>
                                 0%
                             </div>
                         </div>
